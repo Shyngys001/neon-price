26a2ba4240e285d3d613a1944739925b6d821dc8
Shyngys001 <shyngyskhairolla04@mail.ru>
2023-10-25 01:22:15 +0600

    lol

--- index.js
+++ index.js
@@ -1,31 +1,57 @@
+let transparent = document.getElementById("transparent");
+let colored = document.getElementById("colored");
+
+
 function calculatePrice() {
     let baseWidth = document.getElementById('baseWidth').value;
-    let neonLength = document.getElementById('neonLength').value; // convert to cm
+    let neonLength = document.getElementById('neonLength').value; 
     let baseHeight = document.getElementById('baseHeight').value;
     let elementCount = document.getElementById('elementCount').value;
 
-    let totalPrice = baseWidth * 100 + neonLength + baseHeight * elementCount;
+    let totalPrice = baseWidth + neonLength + baseHeight * elementCount;
 
     document.getElementById('estimatedCost').textContent = totalPrice;
 }
 
-// Update slider values dynamically
 let sliders = document.querySelectorAll('input[type=range]');
 
-sliders.forEach(slider => {
-    let span = document.getElementById(slider.id + 'Value');
-    span.textContent = slider.value;
+transparent.addEventListener("change", () => {
+    sliders.forEach(slider => {
+        let span = document.getElementById(slider.id + 'Value');
+        span.textContent = slider.value;
+    
+        slider.oninput = function() {
+            span.textContent = this.value;
+    
+            let baseWidth = document.getElementById('baseWidth').value * 100;
+            let neonLength = document.getElementById('neonLength').value * 8000; 
+            let baseHeight = document.getElementById('baseHeight').value * 50;
+            let elementCount = document.getElementById('elementCount').value * 1500;
+    
+            let totalPrice = baseWidth + neonLength + baseHeight + elementCount*10;
+    
+            document.getElementById('estimatedCost').textContent = totalPrice;
+        }
+    });
+})
 
-    slider.oninput = function() {
-        span.textContent = this.value;
+colored.addEventListener("change", () => {
+    sliders.forEach(slider => {
+        let span = document.getElementById(slider.id + 'Value');
+        span.textContent = slider.value;
+    
+        slider.oninput = function() {
+            span.textContent = this.value;
+    
+            let baseWidth = document.getElementById('baseWidth').value * 5;
+            let neonLength = document.getElementById('neonLength').value * 2000; 
+            let baseHeight = document.getElementById('baseHeight').value * 20;
+            let elementCount = document.getElementById('elementCount').value * 100;
+    
+            let totalPrice = baseWidth + neonLength + baseHeight + elementCount*10;
+    
+            document.getElementById('estimatedCost').textContent = totalPrice;
+        }
+    });
+})
 
-        let baseWidth = document.getElementById('baseWidth').value * 100;
-        let neonLength = document.getElementById('neonLength').value * 8000; 
-        let baseHeight = document.getElementById('baseHeight').value * 50;
-        let elementCount = document.getElementById('elementCount').value * 1500;
-
-        let totalPrice = baseWidth + neonLength + baseHeight + elementCount*10;
-
-        document.getElementById('estimatedCost').textContent = totalPrice;
-    }
-});
